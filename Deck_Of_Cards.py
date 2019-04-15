712080621e926d2db97e6359e6097a7bbc566325
Ruthvik Shandilya <ruthvikshandilya19@gmail.com>
2019-04-14 20:10:59 -0400

    Update Deck_Of_Cards.py

--- Deck_Of_Cards.py
+++ Deck_Of_Cards.py
@@ -1,5 +1,6 @@
 from Deck import Deck
 from flask import Flask
+import os
 
 final_cards=""
 deck = Deck()
@@ -40,6 +41,7 @@ def drawOneCard():
 
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000, debug=True)
+   port = int(os.environ.get('PORT',5000))
+   app.run(host='0.0.0.0', port=port, debug=True)
 
 
